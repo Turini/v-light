85881df6ab451d02b38c360425a7afdf6eb403db
Rodrigo Turini <rdrg.turini@gmail.com>
2015-07-11 00:11:13 -0300

    extracting router component

--- src/main/java/br/com/caelum/vraptorlight/core/Application.java
+++ src/main/java/br/com/caelum/vraptorlight/core/Application.java
@@ -1,11 +1,5 @@
 package br.com.caelum.vraptorlight.core;
 
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 import java.util.function.Function;
 
 import br.com.caelum.vraptorlight.http.HttpMethod;
@@ -20,31 +14,17 @@ import br.com.caelum.vraptorlight.sample.AnalyzeController;
 
 public abstract class Application {
 
-	private final List<UriPattern> mappedUris = new ArrayList<>();
 	private final UriFactory uriFactory = new UriFactory();
+	private final Router router = new Router();
 	
 	protected abstract void setup();
 	
 	public void process(String path, HttpMethod method, VRaptorRequest req, VRaptorResponse res) {
-		//TODO: merge this method with searchAllowedUrisFor?
-		List<UriPattern> mappedUris = searchMappedUrisFor(path);
-		if(mappedUris.isEmpty()) {
-			throw new RuntimeException("404");
-		} 
-		if(mappedUris.size() > 1){
-			String routes = mappedUris.stream().map(UriPattern::getUri).collect(joining(","));
-			throw new RuntimeException("Found ambiguity between routes: " + routes);
+		UriPattern uriPattern = router.searchMappedUrisFor(path, method);
+		if (isParameterizedUri(uriPattern)) {
+			addPathParameters(path, req, uriPattern);
 		}
-		Optional<UriPattern> uri = searchAllowedUrisFor(method, mappedUris);
-		if(uri.isPresent()) {
-			UriPattern uriPattern = uri.get();
-			if (isParameterizedUri(uriPattern)) {
-				addPathParameters(path, req, uriPattern);
-			}
-			uriPattern.getAction().process(req, res);
-		} else {
-			throw new RuntimeException("This path isn't allowed for method " + method);
-		}
+		uriPattern.getAction().process(req, res);
 	}
 
 	private void addPathParameters(String path, VRaptorRequest req, UriPattern uri) {
@@ -58,17 +38,8 @@ public abstract class Application {
 		return ParameterizedUri.class.isAssignableFrom(uriPattern.getClass());
 	}
 
-	private Optional<UriPattern> searchAllowedUrisFor(HttpMethod httpMethod,
-			List<UriPattern> mappedUris) {
-		return mappedUris.stream().filter(uri -> uri.allows(httpMethod)).findFirst();
-	}
-
-	private List<UriPattern> searchMappedUrisFor(String path) {
-		return mappedUris.stream().filter(p -> p.answers(path)).collect(toList());
-	}
-	
 	private void get(String uriPattern, Action processor) {
-		mappedUris.add(uriFactory.uriFor(HttpMethod.GET, uriPattern, processor));
+		router.add(uriFactory.uriFor(HttpMethod.GET, uriPattern, processor));
 	}
 	
 	protected void get(String uriPattern, FixedOutput output) {
@@ -86,7 +57,7 @@ public abstract class Application {
 	}
 	
 	private void post(String uriPattern, Action processor) {
-		mappedUris.add(uriFactory.uriFor(HttpMethod.POST, uriPattern, processor));
+		router.add(uriFactory.uriFor(HttpMethod.POST, uriPattern, processor));
 	}
 	
 	protected void post(String uriPattern, FixedOutput output) {
---
+++ src/main/java/br/com/caelum/vraptorlight/core/Router.java
@@ -0,0 +1,36 @@
+package br.com.caelum.vraptorlight.core;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.caelum.vraptorlight.http.HttpMethod;
+
+public class Router {
+	
+	private final List<UriPattern> mappedUris = new ArrayList<>();
+
+	public UriPattern searchMappedUrisFor(String path, HttpMethod method) {
+		List<UriPattern> uris = mappedUris.stream().filter(p -> p.answers(path)).collect(toList());
+		if(uris.isEmpty()) {
+			throw new RuntimeException("404");
+		} 
+		if(uris.size() > 1){
+			String routes = uris.stream().map(UriPattern::getUri).collect(joining(","));
+			throw new RuntimeException("Found ambiguity between routes: " + routes);
+		}
+		return searchAllowedUrisFor(method, uris);
+	}
+
+	private UriPattern searchAllowedUrisFor(HttpMethod method, List<UriPattern> uris) {
+		return uris.stream().filter(uri -> uri.allows(method)).findFirst().orElseThrow(() -> {
+			return new RuntimeException("This path isn't allowed for method " + method);
+		});
+	}
+	
+	public void add(UriPattern uriFor) {
+		mappedUris.add(uriFor);
+	}
+}
